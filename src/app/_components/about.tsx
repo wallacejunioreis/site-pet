4092b79a849bf5736f6ea4019f8ad7a264adf2ed
wallacejunioreis <wallace.junio.reis@gmail.com>
2025-04-24 18:10:19 +0000

    style(hero) Adicionado o texto, listas com marcadores de serviços

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import about1img from '../../../public/about-1.png'
 import about2img from '../../../public/about-2.png'
+import { Check } from "lucide-react";
+import { WhatsappLogo } from "@phosphor-icons/react/dist/ssr";
 
 
 
@@ -31,6 +33,41 @@ export function About(){
                     priority
                     />
                 </div>
+            </div>
+            <div className="space-y-6">
+                <h2 className="text-4xl font-bold">
+                    SOBRE
+                </h2>
+
+                <p>
+                Aqui, acreditamos que cada pet merece carinho, cuidado e produtos de qualidade. Nosso compromisso é oferecer tudo o que seu companheiro de quatro patas precisa para viver feliz e saudável!
+                </p>
+
+                <ul className="space-y-4 ">
+                    <li className="flex items-center gap-2">
+                        <Check className="text-red-500"/>
+                        Aberto desde 2006
+                    </li>
+                    <li className="flex items-center gap-2">
+                        <Check className="text-red-500"/>
+                        Aberto desde 2006
+                    </li>
+                    <li className="flex items-center gap-2">
+                        <Check className="text-red-500"/>
+                        Temos veterinários.
+                    </li>
+                    <li className="flex items-center gap-2">
+                        <Check className="text-red-500"/>
+                        Qualidade é nossa prioridade
+                    </li>
+                </ul>
+
+                <a href="#"
+                className="bg-[#E84C3D] text-white flex items-center justify-center w-fit gap-2 px-4 py-2 rounded-md">
+                    Contato via Whatsapp
+                    <WhatsappLogo className="w-5 h-5 text-white"/>
+                </a>
+
             </div>
             </div>
         </section>
