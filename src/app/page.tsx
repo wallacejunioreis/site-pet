d45c90ff498fe40363e5ef8a1307ea690b55e06f
wallace.juni.reis@gmail.com <wallace.junio.reis@gmail.com>
2025-04-23 23:33:56 -0300

    feat(about): adiciona página About.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "./_components/hero";
+import { About } from "./_components/about";
 
 export default function Home(){
   return(
     <main>
       <Hero/>
+      <About/>
     </main>
   )
 }
